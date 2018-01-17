c5900cdb685ef6cb4c1ac6be8048f952786f23ea
shushantkumar <shushantkmr2@gmail.com>
2018-01-17 01:09:57 -0800

    products

--- emereNvendre/src/app/app-routing.module.ts
+++ emereNvendre/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProdListComponent } from './prod-list/prod-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'product',
+    component: ProdListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- emereNvendre/src/app/app.module.ts
+++ emereNvendre/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ProdListComponent } from './prod-list/prod-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
