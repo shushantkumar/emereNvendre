ce80ab67ef839be86117cf70019a3ad017ffc748
shushantkumar <shushantkmr2@gmail.com>
2018-01-17 22:40:08 -0800

    login

--- emereNvendre/src/app/app-routing.module.ts
+++ emereNvendre/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProdListComponent } from './prod-list/prod-list.component';
+import { SignInComponent} from './sign-in/sign-in.component'
 
 const routes: Routes = [
 
   {
     path: 'product',
     component: ProdListComponent
+  },
+  {
+    path: 'signin',
+    component: SignInComponent
   }
 ];
 
--- emereNvendre/src/app/app.module.ts
+++ emereNvendre/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdListComponent } from './prod-list/prod-list.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ProdListComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
