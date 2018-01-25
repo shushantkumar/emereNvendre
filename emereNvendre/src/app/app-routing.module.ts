60a5a63671fe704cee94b07d2e311e39737fa491
shushantkumar <shushantkmr2@gmail.com>
2018-01-24 17:31:21 -0800

    Node express

--- emereNvendre/src/app/app-routing.module.ts
+++ emereNvendre/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProdListComponent } from './prod-list/prod-list.component';
-import { SignInComponent} from './sign-in/sign-in.component'
+import { SignInComponent} from './sign-in/sign-in.component';
 
 const routes: Routes = [
 
