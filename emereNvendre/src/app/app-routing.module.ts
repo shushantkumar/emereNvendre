8127656bcab517ae7c8b0c305c8cc2109cc1d21e
shushantkumar <shushantkmr2@gmail.com>
2018-03-01 11:54:36 +0530

    dashboard and home

--- emereNvendre/src/app/app-routing.module.ts
+++ emereNvendre/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ProdListComponent } from './prod-list/prod-list.component';
 import { SignInComponent} from './sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
 
+  {
+    path: '',
+    component:HomeComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
   {
     path: 'product',
     component: ProdListComponent
--- emereNvendre/src/app/app.module.ts
+++ emereNvendre/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProdListComponent } from './prod-list/prod-list.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -16,11 +19,14 @@ import { SignInComponent } from './sign-in/sign-in.component';
     ProdListComponent,
     FooterComponent,
     HeaderComponent,
-    SignInComponent
+    SignInComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
