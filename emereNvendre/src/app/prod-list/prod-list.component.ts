e576f0dff56c50aac865b5ae94545d8f6196ca21
shushantkumar <shushantkmr2@gmail.com>
2018-04-09 00:04:56 +0530

    finale

--- emereNvendre/src/app/prod-list/prod-list.component.ts
+++ emereNvendre/src/app/prod-list/prod-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AllpostsService } from './../allposts/allposts.service';
 
 @Component({
   selector: 'app-prod-list',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdListComponent implements OnInit {
 
-  constructor() { }
+  allpostsdata={};
+  particular;
+  constructor(
+    private allpostsService:AllpostsService
+  ) { }
 
   ngOnInit() {
+    this.getAllPosts(); 
+  }
+
+  getAllPosts(){
+    console.log("Something is going on!");
+    this.allpostsService.getAllPosts().subscribe(
+      (res) =>{
+          console.log(res.products);
+          let response = res.products;
+         
+          this.allpostsdata=response;
+         
+          // console.log(this.allpostsdata);
+          
+          // let response=JSON.parse(res);
+        //   let x,y;
+        //   for (x in response){
+        //     var z = response[x];
+        //   for( y in z) {
+        //     console.log(z[y]);
+        // }}
+                  
+        }, 
+      (err) => console.log(err),
+      () => console.log('done!')
+  );
+
+  }
+  gomodal(something){
+    console.log(something);
+    this.particular=something;
+    console.log(this.particular);
   }
 
 }
